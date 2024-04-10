656546b5d9a27de9b3e28213daa9d4d0f86fb452
ssllaavv <ssllaavv@gmail.com>
2024-04-10 16:46:24 +0300

    Modify URLs in UI tests for production and add UI tests for CD GitHub actions

--- tests/ui/grades.test.js
+++ tests/ui/grades.test.js
@@ -1,7 +1,7 @@
 const { test, expect } = require('@playwright/test');
 
 test('Check grades page', async ({ page }) => {
-    await page.goto('http://localhost:8080/My-Grades');
+    await page.goto('https://ssllaavv-exam-my-grades.onrender.com/My-Grades');
     const list = await page.$('ul');
     expect(list).toBeTruthy();
   });
